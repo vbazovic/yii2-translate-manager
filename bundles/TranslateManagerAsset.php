b7e4456f9a1df6fbf5cb0b7b2fc637532e09f54c
Vladimir Bazovic <vbazovic@gmail.com>
2024-07-23 10:40:11 +0200

    bs5 as requirement

--- bundles/TranslateManagerAsset.php
+++ bundles/TranslateManagerAsset.php
@@ -30,6 +30,6 @@ class TranslateManagerAsset extends AssetBundle
      */
     public $depends = [
         'yii\web\YiiAsset',
-        'yii\bootstrap\BootstrapAsset',
+        'yii\bootstrap5\BootstrapAsset',
     ];
 }
